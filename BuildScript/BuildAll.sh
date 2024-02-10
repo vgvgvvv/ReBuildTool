fd150322265f48681d096927f539c8acafdce449
chen.dong <vgvgvvvdc@gmail.com>
2024-02-11 00:57:19 +0800

    support arm64

--- BuildScript/BuildAll.sh
+++ BuildScript/BuildAll.sh
@@ -3,6 +3,5 @@ cd  %~dp0
 ProjectName=ReBuildTool
 dotnet publish ../$ProjectName/$ProjectName/$ProjectName.csproj -r win-x64 -o ../Binary/Win64/$ProjectName
 dotnet publish ../$ProjectName/$ProjectName/$ProjectName.csproj -r osx-x64 -o ../Binary/Mac64/$ProjectName
+dotnet publish ../$ProjectName/$ProjectName/$ProjectName.csproj -r osx-arm64 -o ../Binary/MacArm64/$ProjectName
 dotnet publish ../$ProjectName/$ProjectName/$ProjectName.csproj -r linux-x64 -o ../Binary/Linux64/$ProjectName
-
-pause
