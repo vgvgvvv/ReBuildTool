a316510a2b58fe1cf5a356e3c58a51a3a8cb3a9f
chen.dong <vgvgvvvdc@gmail.com>
2024-02-11 00:34:20 +0800

    add BuildAll.sh

---
+++ BuildScript/BuildAll.sh
@@ -0,0 +1,8 @@
+cd  %~dp0
+
+ProjectName=ReBuildTool
+dotnet publish ../$ProjectName/$ProjectName/$ProjectName.csproj -r win-x64 -o ../Binary/Win64/$ProjectName
+dotnet publish ../$ProjectName/$ProjectName/$ProjectName.csproj -r osx-x64 -o ../Binary/Mac64/$ProjectName
+dotnet publish ../$ProjectName/$ProjectName/$ProjectName.csproj -r linux-x64 -o ../Binary/Linux64/$ProjectName
+
+pause
