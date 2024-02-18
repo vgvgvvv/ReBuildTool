4588091e11a4d94caf96e564395dc42236f6ee6c
chen.dong <vgvgvvvdc@gmail.com>
2024-02-18 14:26:45 +0800

    update

--- BuildScript/RBTBooster.bat
+++ BuildScript/RBTBooster.bat
@@ -1,8 +1,8 @@
 
 
-rem @echo off
-rem Support by ReBuildTool ! auto update by ReBuildTool.
-rem Dot modify manually !!
+@echo off
+:: Support by ReBuildTool ! auto update by ReBuildTool.
+:: Dot modify manually !!
 
 
 set CURRENT_MODE=%1
@@ -20,31 +20,51 @@ if not defined RBT_HOME (
 
 echo "RBT_HOME is %RBT_HOME%"
 
+echo "main"
+call :main
+
+:main
+    if "%CURRENT_MODE%" == "init" (
+        echo "init ReBuildTool.."
+        call :initReBuildTool
+        exit /b 0
+    ) else if "%CURRENT_MODE%" == "build" (
+        echo "build project.."
+        call :buildProject
+        exit /b 0
+    ) else (
+        echo "invalid arg %CURRENT_MODE%"
+        echo "usage [mode] [targetName]"
+        echo " --init : initialize RBT & setup current directory"
+        echo " --build : build target project"
+        exit /b 1
+    )
+exit 0
+
 :initReBuildTool 
-    if not exist %RBT_HOME%\ (
+    if not exist "%RBT_HOME%\" (
         set REBUILD_REBUILDTOOL="Y"
         mkdir %RBT_HOME%
-    )
-    else (
+    ) else (
         echo "need rebuild ReBuildTool? [Y/N]"
         set /p REBUILD_REBUILDTOOL=
     )
 
-    cd %RBT_HOME%
+    cd /d %RBT_HOME%
+    echo "goto %RBT_HOME% current dir is %cd%"
 
     if "%REBUILD_REBUILDTOOL%"=="Y" (
         
         echo "============= Get ReBuildTool From Git ================"
 
-        if not exist %RBT_HOME%\ReBuildTool\ (
-            echo "clone ReBuildTool"
+        if not exist "%RBT_HOME%\ReBuildTool\" (
+            echo "clone ReBuildTool in %cd%"
             git clone git@github.com:vgvgvvv/ReBuildTool.git
             cd ReBuildTool
             git submodule init
             git submodule update
             cd BuildScript
-        )
-        else (
+        ) else (
             echo "pull ReBuildTool"
             cd ReBuildTool
             git reset --hard
@@ -56,24 +76,22 @@ echo "RBT_HOME is %RBT_HOME%"
         echo "============= Build ReBuildTool ================"
 
         call BuildAll.bat
-    )
-    else (
+    ) else (
         cd "ReBuildTool/BuildScript"
     )
 
     echo "============= Run ReBuildTool ================"
-    if %TARGET_NAME% == "" (
+    if "%TARGET_NAME%" == "" (
         echo "input project name?"
         set /p TARGET_NAME=
     )
 
-    if not %TARGET_NAME% == "" (
+    if not "%TARGET_NAME%" == "" (
         echo "init %TARGET_NAME%"
         cd "../Binary/Win64/ReBuildTool"
 
         call ReBuildTool.exe --ProjectRoot %CURRENT_DIR% --Mode Init --Target %TARGET_NAME% --BoosterSource %CURRENT_SCRIPT_FULL_PATH%
-    )
-    else (
+    ) else (
         echo "no target name skip init"
     )
 exit /b 0
@@ -89,24 +107,6 @@ exit /b 0
     )
 
     call ReBuildTool.exe --ProjectRoot %CURRENT_DIR% --Mode Build --Target %TARGET_NAME% --BoosterSource %CURRENT_SCRIPT_FULL_PATH%
-
 exit /b 0
 
-:main
-    if %CURRENT_MODE%=="init" (
-        call :initReBuildTool
-        exit /b 0
-    )
-    else if %CURRENT_MODE%=="build" (
-        call :buildProject
-        exit /b 0
-    )
-    else (
-        echo "invalid arg %CURRENT_MODE%"
-        echo "usage [mode] [targetName]"
-        echo " --init : initialize RBT & setup current directory"
-        exit /b 1
-    )
-exit /b 0
 
-call :main
