7a49b8e552328a87e67d6a3e11ef5329c92a65fa
ResetOTER <vgvgvvvdc@gmail.com>
2024-02-28 00:41:29 +0800

    support vcpkg

---
+++ ReBuildTool/ReBuildTool/Actions/Vcpkg.cs
@@ -0,0 +1,25 @@
+﻿using NiceIO;
+using ReBuildTool.Common;
+using ReBuildTool.Internal;
+using ReCSharpCommon.Platform;
+using ResetCore.Common;
+
+namespace ReBuildTool.Actions;
+
+public static class Vcpkg
+{
+    public static NPath VcpkgDir => GlobalPaths.IntermediaPath.Combine("Vcpkg").EnsureDirectoryExists();
+    [ActionDefine("Vcpkg.Init")]
+    public static void RunCmd()
+    {
+        Git.GetFromGit("git@github.com:microsoft/vcpkg.git", "vcpkg", VcpkgDir, "2024.02.14");
+        if (PlatformUtils.GetPlatform() == PlatformType.Windows)
+        {
+            SimpleExec.Command.Run(VcpkgDir.Combine("vcpkg", "bootstrap-vcpkg.bat"));
+        }
+        else
+        {
+            SimpleExec.Command.Run(VcpkgDir.Combine("vcpkg", "bootstrap-vcpkg.sh"));
+        }
+    }
+}
