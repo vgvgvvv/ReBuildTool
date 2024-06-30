d7de2c5e97d85bdbfba0b47a5c5d996971011631
ResetOTER <vgvgvvvdc@gmail.com>
2024-06-30 12:55:03 +0800

    update

--- ReBuildTool/ReBuildTool.IDE/CMake/CMakeGenerator.cs
+++ ReBuildTool/ReBuildTool.IDE/CMake/CMakeGenerator.cs
@@ -1,8 +1,31 @@
-﻿using ReCSharpCommon.Result;
+﻿using NiceIO;
+using ReBuildTool.Service.CompileService;
+using ReBuildTool.Service.IDEService.CMake;
+using ReCSharpCommon.Result;
 
 namespace ReBuildTool.ToolChain.IDE.CMake;
 
-public class CMakeGenerator
+public class CMakeLists : ICMakeLists
 {
-	
+	public string name { get; }
+	public NPath fullPath { get; }
+	public bool FlushToFile()
+	{
+		throw new NotImplementedException();
+	}
+}
+
+public class CMakeGenerator : ICMakeGenerator
+{
+	public string Name { get; }
+	public string OutputPath { get; }
+	public ICMakeLists GenerateCMakeProject(ICppSourceProviderInterface source, NPath output)
+	{
+		throw new NotImplementedException();
+	}
+
+	public bool FlushAllCMakeFile()
+	{
+		throw new NotImplementedException();
+	}
 }
