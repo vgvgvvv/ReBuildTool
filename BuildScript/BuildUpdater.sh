519de7739ac6b38eaa4930ce645072471dd6e9dc
ResetOTER <vgvgvvvdc@gmail.com>
2024-09-10 21:46:24 +0800

    udpate

--- BuildScript/BuildUpdater.sh
+++ BuildScript/BuildUpdater.sh
@@ -2,7 +2,7 @@ cd $(dirname $0)
 
 ProjectName=ReBuildTool.Updater
 SlnName=ReBuildTool
-dotnet publish ../$SlnName/$ProjectName/$ProjectName.csproj -r win-x64 -o ../Binary/Win64/$ProjectName
-dotnet publish ../$SlnName/$ProjectName/$ProjectName.csproj -r osx-x64 -o ../Binary/Mac64/$ProjectName
-dotnet publish ../$SlnName/$ProjectName/$ProjectName.csproj -r osx-arm64 -o ../Binary/MacArm64/$ProjectName
-dotnet publish ../$SlnName/$ProjectName/$ProjectName.csproj -r linux-x64 -o ../Binary/Linux64/$ProjectName
+dotnet publish ../$SlnName/$ProjectName/$ProjectName.csproj -r win-x64 --self-contained -o ../Binary/Win64/$ProjectName
+dotnet publish ../$SlnName/$ProjectName/$ProjectName.csproj -r osx-x64 --self-contained -o ../Binary/Mac64/$ProjectName
+dotnet publish ../$SlnName/$ProjectName/$ProjectName.csproj -r osx-arm64 --self-contained -o ../Binary/MacArm64/$ProjectName
+dotnet publish ../$SlnName/$ProjectName/$ProjectName.csproj -r linux-x64 --self-contained -o ../Binary/Linux64/$ProjectName
