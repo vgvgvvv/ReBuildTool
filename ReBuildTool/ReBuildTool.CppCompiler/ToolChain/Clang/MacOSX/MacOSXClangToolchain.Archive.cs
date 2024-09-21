e8d5be0fee7725d486b822f241b5ee1f70553f08
ResetOTER <vgvgvvvdc@gmail.com>
2024-09-22 00:48:03 +0800

    support linux archive

--- ReBuildTool/ReBuildTool.CppCompiler/ToolChain/Clang/MacOSX/MacOSXClangToolchain.Archive.cs
+++ ReBuildTool/ReBuildTool.CppCompiler/ToolChain/Clang/MacOSX/MacOSXClangToolchain.Archive.cs
@@ -7,6 +7,14 @@ public partial class MacOSXClangToolchain
     protected override IEnumerable<string> ArchiveArgsFor(CppArchiveUnit unit)
     {
         yield return "rcs";
+        
+        var linkBuilder = unit.ArchiveArgsBuilder;
+        
+        foreach (var argument in linkBuilder.GetAllArguments())
+        {
+            yield return argument;
+        }
+        
         yield return unit.OutputPath.InQuotes();
 		
         var lines = File.ReadLines(unit.ResponseFile);
--- ReBuildTool/ReBuildTool.CppCompiler/ToolChain/Gcc/GccToolChain.Archive.cs
+++ ReBuildTool/ReBuildTool.CppCompiler/ToolChain/Gcc/GccToolChain.Archive.cs
@@ -16,39 +16,22 @@ public partial class GccToolChain
 	
     private IEnumerable<string> ArchiveArgsFor(CppArchiveUnit unit)
     {
-        yield return $"-o";
-        yield return unit.OutputPath.InQuotes();
-		
-        foreach (var defaultLinkFlag in DefaultArchiveFlags(unit))
-        {
-            yield return defaultLinkFlag;
-        }
-		
-        yield return "@" + unit.ResponseFile.InQuotes();
-    }
-	
-    protected IEnumerable<string> DefaultArchiveFlags(CppArchiveUnit cppArchiveUnit)
-    {
-        var linkBuilder = cppArchiveUnit.ArchiveArgsBuilder;
+        yield return "rcs";
+        
+        var linkBuilder = unit.ArchiveArgsBuilder;
         
         foreach (var argument in linkBuilder.GetAllArguments())
         {
             yield return argument;
         }
-		
-        foreach (var staticLibrary in cppArchiveUnit.StaticLibraries)
-        {
-            yield return staticLibrary.ToNPath().InQuotes();
-        }
-		
-        foreach (var libraryPath in cppArchiveUnit.LibraryPaths)
-        {
-            yield return $"-L{libraryPath.InQuotes()}";
-        }
         
-        foreach (var libpath in ToolChainLibraryPaths())
+        yield return unit.OutputPath.InQuotes();
+		
+        var lines = File.ReadLines(unit.ResponseFile);
+        foreach (var line in lines)
         {
-            yield return $"-L{libpath.InQuotes()}";
+            yield return $"\"{line}\"";
         }
     }
+
 }
