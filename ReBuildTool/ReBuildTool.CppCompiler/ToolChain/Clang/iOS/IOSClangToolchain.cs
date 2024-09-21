744536e1609e7f5aaf3d240aed0be386b6d5100e
chen.dong <vgvgvvvdc@gmail.com>
2024-09-22 00:16:46 +0800

    support simulator sdk

--- ReBuildTool/ReBuildTool.CppCompiler/ToolChain/Clang/iOS/IOSClangToolchain.cs
+++ ReBuildTool/ReBuildTool.CppCompiler/ToolChain/Clang/iOS/IOSClangToolchain.cs
@@ -15,3 +15,15 @@ public class IOSClangToolchain : MacOSXClangToolchain
 	private XCodeSDK IPhoneXCodeSdk { get; }
 	
 }
+
+public class IOSSimulatorClangToolchain : MacOSXClangToolchain
+{
+	public IOSSimulatorClangToolchain(BuildConfiguration configuration, Architecture arch) : base(configuration, arch)
+	{
+		IPhoneXCodeSdk = new XCodeSDK("/Applications/Xcode.app".ToNPath(), XCodePlatformSDK.ApplePlatform.iPhoneSimulator);
+	}
+
+	protected override ClangSDK ClangSdk => IPhoneXCodeSdk;
+	private XCodeSDK IPhoneXCodeSdk { get; }
+	
+}
